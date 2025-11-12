6797cae7b3c17d596c1faf62674420aea8d5a92b
Lowie <lowie.klaasen@gmail.com>
2025-11-12 14:27:23 +0100

    Mission 3.1

---
+++ backend/src/main/java/ap/student/backend/CommunicationSystems.java
@@ -0,0 +1,92 @@
+package ap.student.backend;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.format.TextStyle;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+@RestController()
+@RequestMapping("/communication-systems")
+public class CommunicationSystems {
+
+    private final WebClient webClient = WebClient.create();
+
+    @GetMapping()
+    public ResponseEntity<String> check() {
+        return ResponseEntity.ok("Communication Systems");
+    }
+
+    @GetMapping("HandoffProtocolHandler")
+    public ResponseEntity<Map<String, Object>> handoff() {
+
+        String missionId = "4439a9f8-77eb-461b-9dcf-29e57a40fcc4";
+
+        int year = 2002;
+        int month = 9;
+
+        YearMonth ym = YearMonth.of(year, month);
+
+        LocalDate firstDay = ym.atDay(1);
+        LocalDate lastDay = ym.atEndOfMonth();
+
+        String firstDayName = firstDay.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.ENGLISH).toUpperCase();
+        String lastDayName = lastDay.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.ENGLISH).toUpperCase();
+
+
+//        Map<String, Object> body = new HashMap<>();
+//        body.put("result", firstDayName + "-" + lastDayName);
+//
+//        return ResponseEntity.ok(body);
+
+        try {
+            String response = CallAPI(missionId, firstDayName + "-" + lastDayName);
+
+            if (response != null && response.contains("\"missionSolved\":true")) {
+                Map<String, Object> body = new HashMap<>();
+                body.put("found", true);
+                body.put("result", firstDayName + "-" + lastDayName);
+                body.put("externalResponse", response);
+
+                return ResponseEntity.ok(body);
+            }
+
+        } catch (WebClientResponseException e) {
+            // ignore 4xx or 5xx errors and continue
+        }
+
+        Map<String, Object> body = new HashMap<>();
+        body.put("found", false);
+
+        return ResponseEntity.ok(body);
+
+    }
+
+
+
+
+
+    // region Private Methods
+
+    private String CallAPI(String missionId, String result) {
+        String url = "https://htf.collide.be/missions/" + missionId + "/solution";
+
+        return webClient.post()
+                .uri(url + "?result=" + result)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+    }
+
+    // endregion
+
+}
