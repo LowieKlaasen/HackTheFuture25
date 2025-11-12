5029f88bb8f559b096ece2cb29e072bcb527c47e
Lowie <lowie.klaasen@gmail.com>
2025-11-12 15:47:14 +0100

    landing page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { AquaInfrastructureComponent } from './aqua-infrastructure/aqua-infrastructure.component';
 import { CommonDividerComponent } from './common-divider/common-divider.component';
 import { DecodeComponent } from './decode/decode.component';
+import {Home} from './home/home';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/password', pathMatch: 'full' },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'password', component: AquaInfrastructureComponent },
     { path: 'divider', component: CommonDividerComponent },
     { path: 'decode', component: DecodeComponent },
-    { path: '**', redirectTo: '/password' }
+    { path: 'home', component: Home },
+    { path: '**', redirectTo: '/home' }
 ];
